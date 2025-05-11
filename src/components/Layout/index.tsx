efc30088950f942b61eb009995a36ca63e52413d
piro0919 <piro.haniwa@gmail.com>
2025-05-11 18:40:16 +0900

    chore: set es2017 target and fix layout return type

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,13 @@
 "use client";
 import useShowWindowSize from "@/hooks/useShowWindowSize";
+import React from "react";
 import { ReactNode } from "react";
 
 export type LayoutProps = {
   children: ReactNode;
 };
 
-export default function Layout({ children }: LayoutProps): JSX.Element {
+export default function Layout({ children }: LayoutProps): React.JSX.Element {
   useShowWindowSize({
     // disable: process.env.NODE_ENV === "production",
   });
